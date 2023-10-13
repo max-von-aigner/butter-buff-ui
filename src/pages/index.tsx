f4be4d985ecb97758b648e8ba2066286b62105d4
Max-von-Aiger <von.aigner@gmail.com>
2023-10-13 17:05:53 +0200

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="h-screen w-full grid grid-rows-7 bg-black">
+    <main className="h-screen w-full grid grid-rows-7 bg-custom-blue relative">
       <motion.div
-        className="box row-start-3 row-end-4 place-self-center"
+        className="box row-start-3 row-end-4 place-self-center absolute top-80"
         whileHover={{ scale: 1.1 }}
         transition={{ type: "spring", stiffness: 400, damping: 10 }}
       >
@@ -18,13 +18,3 @@ export default function Home() {
     </main>
   );
 }
-
-// export default function Home() {
-//   return (
-
-//       <div className="">
-//         <DropdownMenuHamster />
-//       </div>
-//     </main>
-//   );
-// }
