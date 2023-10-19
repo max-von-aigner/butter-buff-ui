e7b85058b475227c7cef458a1894ca36d6c3786e
Max-von-Aiger <von.aigner@gmail.com>
2023-10-19 14:07:35 +0200

    add stylewrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,17 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import { DropdownMenuHamster } from "@/react-components/DropdownMenu";
-import { frame, motion } from "framer-motion";
 import DemoPaymentMethod from "./paymentmethod";
 import TagPicker from "@/react-components/TagPicker";
 import PaymentMethodCard from "@/react-components/PaymentMethodCard";
+import DropdownMenuAnimated from "@/react-components/DropdownMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className="h-screen w-full overflow-y-scroll snap-y snap-mandatory"
-      // style={{ scrollSnapType: "y mandatory" }}
-    >
-      {/* <DropdownMenuHamster /> */}
-
+    <main className="h-screen w-full overflow-y-scroll snap-y snap-mandatory">
+      <DropdownMenuAnimated />
       <TagPicker />
       <PaymentMethodCard />
-      <DemoPaymentMethod />
     </main>
   );
 }
